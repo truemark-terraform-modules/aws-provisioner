8d0346e1902181e75da23fd72d5f007e95752af5
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-12 06:11:23 -0600

    Fixing formatting

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  s3_bucket = var.s3_bucket == null ? "${data.aws_caller_identity.current.account_id}-terraform" : var.s3_bucket
+  s3_bucket       = var.s3_bucket == null ? "${data.aws_caller_identity.current.account_id}-terraform" : var.s3_bucket
   trusted_account = var.trusted_account == null ? data.aws_caller_identity.current.account_id : var.trusted_account
 }
 
 resource "aws_iam_user" "provisioner" {
   count = var.create_user ? 1 : 0
-  name = var.name
+  name  = var.name
 }
 
-data aws_iam_policy_document "assume_role_policy" {
+data "aws_iam_policy_document" "assume_role_policy" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
     principals {
       identifiers = ["arn:aws:iam::${local.trusted_account}:root"]
@@ -22,10 +22,10 @@ data aws_iam_policy_document "assume_role_policy" {
 }
 
 resource "aws_iam_role" "provisioner" {
-  count = var.create_role ? 1 : 0
-  name = var.name
-  description = var.description
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  count                = var.create_role ? 1 : 0
+  name                 = var.name
+  description          = var.description
+  assume_role_policy   = data.aws_iam_policy_document.assume_role_policy.json
   max_session_duration = var.max_session_duration
 }
 
@@ -35,105 +35,105 @@ resource "aws_iam_role" "provisioner" {
 
 data "aws_iam_policy_document" "s3_provisioner" {
   statement {
-    sid = "AllowBucketList"
-    effect = "Allow"
-    actions = ["s3:ListAllMyBuckets", "s3:GetBucketLocation"]
+    sid       = "AllowBucketList"
+    effect    = "Allow"
+    actions   = ["s3:ListAllMyBuckets", "s3:GetBucketLocation"]
     resources = ["arn:aws:s3:::*"]
   }
   statement {
-    sid = "AllowListBucket"
-    effect = "Allow"
-    actions = ["s3:ListBucket"]
+    sid       = "AllowListBucket"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${local.s3_bucket}"]
   }
   statement {
-    sid = "AllowPath"
-    effect = "Allow"
-    actions = ["s3:GetObject", "s3:PutObject", "s3:ListObjects"]
+    sid       = "AllowPath"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:ListObjects"]
     resources = ["arn:aws:s3:::${local.s3_bucket}/${var.s3_prefix}/*"]
   }
   statement {
-    sid = "AllowWorkspacePath"
-    effect = "Allow"
-    actions = ["s3:GetObject", "s3:PutObject", "s3:ListObjects"]
+    sid       = "AllowWorkspacePath"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:ListObjects"]
     resources = ["arn:aws:s3:::${local.s3_bucket}/env:/*/${var.s3_prefix}/*"]
   }
   statement {
-    sid = "AllowDynamo"
-    effect = "Allow"
-    actions = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:DeleteItem"]
+    sid       = "AllowDynamo"
+    effect    = "Allow"
+    actions   = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:DeleteItem"]
     resources = ["arn:aws:dynamodb:*:${data.aws_caller_identity.current.account_id}:table/${local.s3_bucket}"]
   }
 }
 
 resource "aws_iam_policy" "s3_provisioner" {
-  count = var.s3_prefix != null && (var.create_user || var.create_role) ? 1 : 0
-  name = "${var.name}-s3-terraform"
-  path = var.path
+  count       = var.s3_prefix != null && (var.create_user || var.create_role) ? 1 : 0
+  name        = "${var.name}-s3-terraform"
+  path        = var.path
   description = "Allows access to ${local.s3_bucket}/${var.s3_prefix}"
-  policy = data.aws_iam_policy_document.s3_provisioner.json
+  policy      = data.aws_iam_policy_document.s3_provisioner.json
 }
 
 resource "aws_iam_user_policy_attachment" "s3_provisioner" {
-  count = var.s3_prefix != null && var.create_user ? 1 : 0
+  count      = var.s3_prefix != null && var.create_user ? 1 : 0
   policy_arn = aws_iam_policy.s3_provisioner[count.index].arn
-  user = aws_iam_user.provisioner[count.index].name
+  user       = aws_iam_user.provisioner[count.index].name
 }
 
 resource "aws_iam_role_policy_attachment" "s3_provisioner" {
-  count = var.s3_prefix != null && var.create_role ? 1 : 0
+  count      = var.s3_prefix != null && var.create_role ? 1 : 0
   policy_arn = aws_iam_policy.s3_provisioner[count.index].arn
-  role = aws_iam_role.provisioner[count.index].name
+  role       = aws_iam_role.provisioner[count.index].name
 }
 
 resource "aws_iam_policy" "provisioner" {
-  count = var.policy != null ? 1 : 0
-  name = var.name
-  path = var.path
+  count       = var.policy != null ? 1 : 0
+  name        = var.name
+  path        = var.path
   description = "Access policy for IAM user ${var.name}. Created and attached by TrueMark terraform module terraform-aws-provisioner."
-  policy = var.policy
+  policy      = var.policy
 }
 
 resource "aws_iam_user_policy_attachment" "provisioner" {
-  count = var.policy != null && var.create_user ? 1 : 0
+  count      = var.policy != null && var.create_user ? 1 : 0
   policy_arn = aws_iam_policy.provisioner[count.index].arn
-  user = aws_iam_user.provisioner[count.index].name
+  user       = aws_iam_user.provisioner[count.index].name
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner" {
-  count = var.policy != null && var.create_role ? 1 : 0
+  count      = var.policy != null && var.create_role ? 1 : 0
   policy_arn = aws_iam_policy.provisioner[count.index].arn
-  role = aws_iam_role.provisioner[count.index].name
+  role       = aws_iam_role.provisioner[count.index].name
 }
 
 resource "aws_iam_policy" "provisioner_n" {
-  count = length(var.policies)
-  name = "${var.name}-${count.index}"
-  path = var.path
+  count       = length(var.policies)
+  name        = "${var.name}-${count.index}"
+  path        = var.path
   description = "Access policy for IAM user ${var.name}"
-  policy = var.policies[count.index]
+  policy      = var.policies[count.index]
 }
 
 resource "aws_iam_user_policy_attachment" "provisioner_n" {
-  count = var.create_user ? length(var.policies) : 0
+  count      = var.create_user ? length(var.policies) : 0
   policy_arn = aws_iam_policy.provisioner_n[count.index].arn
-  user = join("", aws_iam_user.provisioner.*.name)
+  user       = join("", aws_iam_user.provisioner.*.name)
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_n" {
-  count = var.create_role ? length(var.policies) : 0
+  count      = var.create_role ? length(var.policies) : 0
   policy_arn = aws_iam_policy.provisioner_n[count.index].arn
-  role = join("", aws_iam_role.provisioner.*.name)
+  role       = join("", aws_iam_role.provisioner.*.name)
 }
 
 resource "aws_iam_user_policy_attachment" "attachments" {
-  count = var.create_user ? length(var.policy_arns) : 0
+  count      = var.create_user ? length(var.policy_arns) : 0
   policy_arn = var.policy_arns[count.index]
-  user = join("", aws_iam_user.provisioner.*.name)
+  user       = join("", aws_iam_user.provisioner.*.name)
 }
 
 resource "aws_iam_role_policy_attachment" "attachments" {
-  count = var.create_role ? length(var.policy_arns) : 0
+  count      = var.create_role ? length(var.policy_arns) : 0
   policy_arn = var.policy_arns[count.index]
-  role = join("", aws_iam_role.provisioner.*.name)
+  role       = join("", aws_iam_role.provisioner.*.name)
 }
--- variables.tf
+++ variables.tf
@@ -1,67 +1,67 @@
 variable "name" {
   description = "Name of the IAM user/role to create"
-  type = string
+  type        = string
 }
 
 variable "description" {
   description = "Set a description for the purpose of the IAM user role"
-  type = string
-  default = "User access role created by TrueMark terraform module terraform-aws-provisioner."
+  type        = string
+  default     = "User access role created by TrueMark terraform module terraform-aws-provisioner."
 }
 
 variable "create_user" {
   description = "Default is True to create an IAM user"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "create_role" {
   description = "Default is False. Set to True to create an IAM role"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "s3_bucket" {
   description = "Name of the terraform bucket holding terraform state information. Defaults to <account>-terraform."
-  default = null
-  type = string
+  default     = null
+  type        = string
 }
 
 variable "s3_prefix" {
   description = "Optional path prefix inside the terraform S3 bucket to grant access to."
-  default = null
-  type = string
+  default     = null
+  type        = string
 }
 
 variable "policy" {
   description = "The policy to create and apply to the IAM user."
-  default = null
-  type = string
+  default     = null
+  type        = string
 }
 
 variable "policies" {
   description = "The policies to create and apply to the IAM user."
-  default = []
-  type = list(string)
+  default     = []
+  type        = list(string)
 }
 
 variable "policy_arns" {
   description = "Additional policies to attach to the IAM user."
-  default = []
-  type = list(string)
+  default     = []
+  type        = list(string)
 }
 
 variable "path" {
   default = "/"
-  type = string
+  type    = string
 }
 
 variable "trusted_account" {
   default = null
-  type = string
+  type    = string
 }
 
 variable "max_session_duration" {
-  default = "7200"
+  default     = "7200"
   description = "The maximum session duration (in seconds) that you want to set for the specified role. If you do not specify a value for this setting, the default maximum of two hour is applied. This setting can have a value from 1 hour to 12 hours."
 }
